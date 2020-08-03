b713b6e868cec75c775a2405511b5153e3442603
Artem Nikiforovskiy <nicktlibra@gmail.com>
2020-08-03 22:46:10 +0300

    добавил промремонт

---
+++ simple_promremont/js/script.js
@@ -0,0 +1,28 @@
+// let arr = ['red', 'blue', 'green', 'white', 'black'];
+
+// arr.forEach(function (item, i, arr) {
+// 	console.log(`№${i}: ${item}`);
+// })
+
+// let b = {
+// 	one: 'red',
+// 	two: 'blue',
+// 	three: 'green',
+// 	fore: {
+// 		eee: 111,
+// 		bbb: 222
+// 	}
+// }
+
+// let {eee} = b.fore;
+// console.log(eee);
+
+// for(let key in b) {
+// 	if(typeof(b[key]) === 'Object') {
+// 		for(let i in b[key]) {
+// 			console.log(`${i} = ${b[key][i]}`);
+// 		} 
+// 		} else {
+// 			console.log(`${key} = ${b[key]}`);
+// 	}
+// }
